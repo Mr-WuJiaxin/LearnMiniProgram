e19e46ecd0a11dec4cd95b97ac42583e08ba8ad6
未名烏 <2317349886@qq.com>
2020-10-30 22:01:09 +0800

    知识点3

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,27 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'world',
+    age: 18,
+    student:[
+      {id:110,name:'kobe',age:18},
+      {id:120,name:'mini',age:19},
+      {id:130,name:'daming',age:20}
+    ],
+    counter:0,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleBtnClick(){
+    //1.错误做法：界面是不会刷新的
+    // this.data.counter +=1,
+    //console.log(this.data.counter)
+    //2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+    },
+    handleSubteraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
